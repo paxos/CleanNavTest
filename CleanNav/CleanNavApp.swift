70e4cdbd5ae61560b5acc5532fc6dbb254fa3126
Patrick Dinger <121539+paxos@users.noreply.github.com>
2025-07-21 12:28:15 -0700

    Initial Commit

---
+++ CleanNav/CleanNavApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanNavApp.swift
+//  CleanNav
+//
+//  Created by Patrick Dinger on 7/21/25.
+//
+
+import SwiftUI
+
+@main
+struct CleanNavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CleanNav/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CleanNav
+//
+//  Created by Patrick Dinger on 7/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
